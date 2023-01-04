5ca74e6b92e04cafc29ea54a4c441a817769f1d8
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-01-04 17:39:46 +0100

    mise en forme profil entreprise

--- src/app/components/profil-entreprise/profil-entreprise.component.ts
+++ src/app/components/profil-entreprise/profil-entreprise.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { EditSocietyComponent } from 'src/app/modals/edit-society/edit-society.component';
+import { FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-profil-entreprise',
   templateUrl: './profil-entreprise.component.html',
@@ -7,9 +11,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProfilEntrepriseComponent implements OnInit {
 
+  
+  
   @Input() profilEntreprise!: any
 
-  constructor() { }
+  constructor(
+    private _dialog: MatDialog,
+    ) { }
 
   ngOnInit(): void {
     
@@ -18,10 +26,24 @@ export class ProfilEntrepriseComponent implements OnInit {
 
   onSubmit(): void {
     
-    
+
 
   }
 
+  onEdit() {
+    let dialogRef = this._dialog.open(EditSocietyComponent, {
+      width: "50%",
+      height: "80%",
+      data: this.profilEntreprise
+    })
+    dialogRef.afterClosed().subscribe((profil: any) => {
+      this.profilEntreprise = profil.profil.newDatas
+      console.log(this.profilEntreprise);
+      // this.newValues = profil.profil.newDatas
+      // console.log(this.newValues);
+    })
+
+  }
   
 
 
