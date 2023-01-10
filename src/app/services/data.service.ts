7a4175edbff8bd522e22f546547d016416e4c796
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-01-10 16:53:33 +0100

    ajout service pour test countries

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient, HttpEvent } from '@angular/common/http';
+import { Observable, ObservableLike } from 'rxjs';
 
 import { Contrat } from '../models/contrat';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,18 +10,24 @@ import { environment } from 'src/environments/environment';
 })
 export class DataService {
 
-private _apiUrl = `${environment.API_URL}api/contracts`
+  private _apiUrl = `${environment.API_URL}api/contracts`
+  urlEuropeCountries = "https://restcountries.com/v3.1/region/europe"
 
   constructor(
-    private _http : HttpClient
-    ) { }
+    private _http: HttpClient
+  ) { }
 
-    createContract(contrat:Contrat): Observable<any> {
-      return this._http.post(`${this._apiUrl}/createContract`,contrat)
-    }
+  getCountries(): Observable<any> {
+    return this._http.get(this.urlEuropeCountries)
+  }
 
-    searchAll(): Observable<any> {
-      return this._http.get(`${this._apiUrl}/allContracts`)
-    }
-  
+
+  createContract(contrat: Contrat): Observable<any> {
+    return this._http.post(`${this._apiUrl}/createContract`, contrat)
+  }
+
+  searchAll(): Observable<any> {
+    return this._http.get(`${this._apiUrl}/allContracts`)
+  }
+
 }
