cb9cf81125caedcb54c6b3ba88c5a8fa2a81e1a6
MacMini de Sulayman <macmini@mac-mini-de-macmini.home>
2023-01-25 12:46:52 +0100

    maj module pour date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { MAT_RADIO_DEFAULT_OPTIONS } from '@angular/material/radio';
-import { NgModule } from '@angular/core';
+import {
+  NgModule
+} from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './modules/shared/shared.module';
 import { TokenInterceptorProvider } from './helpers/header.interceptor';
@@ -13,7 +15,7 @@ import { TokenInterceptorProvider } from './helpers/header.interceptor';
   declarations: [
     AppComponent,
     AccueilComponent,
-   
+
   ],
   imports: [
     AppRoutingModule,
@@ -24,10 +26,13 @@ import { TokenInterceptorProvider } from './helpers/header.interceptor';
 
   ],
   providers: [TokenInterceptorProvider,
+
     {
       provide: MAT_RADIO_DEFAULT_OPTIONS,
       useValue: { color: 'primary' },
     }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
