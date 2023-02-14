b556884387dcc7a2581a0ef578c50be68d2bb36b
Sulayman <sirikone@hotmail.com>
2023-02-14 14:03:55 +0100

    test unitaires préparation

--- src/app/helpers/auth.guard.spec.ts
+++ src/app/helpers/auth.guard.spec.ts
@@ -1,12 +1,19 @@
-import { TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 
+import { ActivatedRoute } from '@angular/router';
 import { AuthGuard } from './auth.guard';
+import { DataService } from '../services/data.service';
+import { FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { TestBed } from '@angular/core/testing';
+import { UsersService } from '../services/users.service';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [UsersService, DataService, { provide: ActivatedRoute, useValue: {} }, { provide: HttpClient, useValue: {} }, { provide: MAT_DIALOG_DATA, useValue: {} }, { provide: MatSnackBar, useValue: {} }, { provide: FormBuilder, useValue: {} }, { provide: MatDialog, useValue: {} }] });
     guard = TestBed.inject(AuthGuard);
   });
 
