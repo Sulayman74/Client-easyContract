41e61dd34cc92e8f6378fd273d22badd903f293e
MacMini de Sulayman <macmini@Mac-mini-de-MacMini.local>
2023-02-28 13:12:56 +0100

    création du contenu component

--- src/app/components/mes-salaries/mes-salaries.component.ts
+++ src/app/components/mes-salaries/mes-salaries.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { UsersService } from 'src/app/services/users.service';
+
 @Component({
   selector: 'app-mes-salaries',
   templateUrl: './mes-salaries.component.html',
@@ -7,10 +9,21 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class MesSalariesComponent implements OnInit {
   @Input() profilEntreprise !: any
+  displayedColumns = ['name', 'salary'];
+  dataSource = [
+    { name: 'John', salary: '$5000' },
+    { name: 'Jane', salary: '$4000' },
+    { name: 'Jill', salary: '$3000' },
+  ];
 
-  constructor() { }
+
+  constructor(private _userService : UsersService) { }
 
   ngOnInit(): void {
+    this._userService.getMyWorkers().subscribe((worker:any)=>{
+      console.log("mon worker",worker);
+    })
+    console.log("profilEntrprise",this.profilEntreprise);
   }
 
 }
